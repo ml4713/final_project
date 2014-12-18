92afdfd3cab734174987896022b9a08dfc605077
Mengfei Li <ml4713@nyu.edu>
2014-12-17 22:11:49 -0500

    trial

--- ml4713/data_visual/data_clean_visual.py
+++ ml4713/data_visual/data_clean_visual.py
@@ -31,15 +31,12 @@ def clean_data_for_visual(f_path):
     head = ['age','workclass','fnlwgt','education','education-num','martial-status','occupation','relationship','race','sex','capital-gain','capital-loss','hours-per-week','native-country','y']
     file_path_validated = filePath_validation(f_path)
     uncleaned_data = pd.read_csv(file_path_validated, header = None)       
-    if uncleaned_data.shape == (32562,15): #make sure it is the correct dataset
-       uncleaned_data.columns = head
-       col_to_edit = ['workclass','education','martial-status','occupation','relationship','sex','native-country','race','y']
-       for col in col_to_edit:
-           uncleaned_data[col] = uncleaned_data[col].map(lambda x:str(x).strip()) #remove whitespace in categorical values
-           cleaned_data = uncleaned_data[uncleaned_data['y']!='nan']
-       return cleaned_data
-    else:
-       raise ReadFileError('Not correct dataset')
+    uncleaned_data.columns = head
+    col_to_edit = ['workclass','education','martial-status','occupation','relationship','sex','native-country','race','y']
+    for col in col_to_edit:
+        uncleaned_data[col] = uncleaned_data[col].map(lambda x:str(x).strip()) #remove whitespace in categorical values
+        cleaned_data = uncleaned_data[uncleaned_data['y']!='nan']
+    return cleaned_data
 
         
            
